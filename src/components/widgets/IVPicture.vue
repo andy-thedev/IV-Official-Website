5537a6649a0c2ad57e9cffd0f7e71096c828d5ce
Andy Jung <wontaethedev@gmail.com>
2024-10-13 19:02:39 +0900

    Fix bug

--- src/components/widgets/IVPicture.vue
+++ src/components/widgets/IVPicture.vue
@@ -1,6 +1,6 @@
 <template>
   <picture>
-    <source :srcset="mobileSrc" media="(max-width: 1280)" />
+    <source :srcset="mobileSrc" media="(max-width: 1280px)" />
     <source :srcset="hdSrc" media="(max-width: 1920px)" />
     <source :srcset="qhdSrc" media="(max-width: 2560px)" />
     <source :srcset="fourkSrc" media="(min-width: 3840px)" />
